0b4ab3d381e04eba0f3b12a9586ac195287f53d0
Arif Azahar <azahararif@gmail.com>
2020-10-02 15:29:07 +0800

    working: login system + basic navigation

--- app/src/main/java/com/example/entreprenuershipproject/NavigationActivity.java
+++ app/src/main/java/com/example/entreprenuershipproject/NavigationActivity.java
@@ -1,17 +1,34 @@
 package com.example.entreprenuershipproject;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class NavigationActivity extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
 
+        BottomNavigationView bottomNavigation  = findViewById(R.id.nav_view);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigation, navController);
     }
+
 }
