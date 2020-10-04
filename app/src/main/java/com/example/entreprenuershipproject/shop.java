477b9916721ca1fa3695d8c9e191895e34e47686
Arif Azahar <azahararif@gmail.com>
2020-10-04 11:43:30 +0800

    031020 database feature working - https://firebase.google.com/docs/database/android/read-and-write

---
+++ app/src/main/java/com/example/entreprenuershipproject/shop.java
@@ -0,0 +1,43 @@
+package com.example.entreprenuershipproject;
+
+import androidx.lifecycle.ViewModel;
+
+public class shop extends ViewModel {
+    public String shopName, shopStatus, shopImage;
+
+    public shop() {
+    }
+
+    public shop(String shopName, String shopStatus, String shopImage) {
+        this.shopName = shopName;
+        this.shopStatus = shopStatus;
+        this.shopImage = shopImage;
+    }
+
+    //
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public String getShopStatus() {
+        return shopStatus;
+    }
+
+    public void setShopStatus(String shopStatus) {
+        this.shopStatus = shopStatus;
+    }
+
+    public String getShopImage() {
+        return shopImage;
+    }
+
+    public void setShopImage(String shopImage) {
+        this.shopImage = shopImage;
+    }
+
+
+}
